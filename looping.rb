d3ee6e15ef332e94071f9c14d2583de6bcf6de9f
hkato4188 <hkato@sas.upenn.edu>
2021-03-25 10:41:12 -0400

    Done.

--- looping.rb
+++ looping.rb
@@ -1,5 +1,10 @@
 def looping
 	#your code here
+  loop do
+    puts "Never pass a school bus if it has flashing red lights"
+  end
 end
 
 #call your method here
+looping
+#infinite loops can be exited with Control + c in terminal
